cc532f9e2b94768f1f9324a9561212a461c9721a
NguyenMinhSon216 <97439020+NguyenMinhSon216@users.noreply.github.com>
2022-01-17 16:49:57 +0700

    Add files via upload

---
+++ BaiThucHanhJava/BaiThucHanh1/src/App.java
@@ -0,0 +1,9 @@
+import java.util.Scanner;
+public class App {
+    public static void main(String[] args) throws Exception {
+        Scanner sc = new Scanner(System.in);
+        String strHoTen ="";
+        System.out.println("Nhập họ tên: ");
+        System.out.println("Xin chao" +strHoTen);
+    }
+}
