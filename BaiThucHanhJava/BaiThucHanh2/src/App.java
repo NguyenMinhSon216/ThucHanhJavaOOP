4720394369c4c80a9efa4b57dbcf43b8ded5c18e
NguyenMinhSon216 <97439020+NguyenMinhSon216@users.noreply.github.com>
2022-01-17 16:48:32 +0700

    Delete App.java

--- BaiThucHanhJava/BaiThucHanh2/src/App.java
+++
@@ -1,9 +0,0 @@
-import java.util.Scanner;
-public class App {
-    public static void main(String[] args) throws Exception {
-        Scanner sc = new Scanner(System.in);
-        String strHoTen ="";
-        System.out.println("Nhập họ tên: ");
-        System.out.println("Xin chao" +strHoTen);
-    }
-}
