217049376fa31ea513c2a3d9cded6cdfd2673bd0
NguyenMinhSon216 <97439020+NguyenMinhSon216@users.noreply.github.com>
2022-02-21 17:50:04 +0700

    Add files via upload

---
+++ BaiThucHanh2102-51/src/App51.java
@@ -0,0 +1,11 @@
+import java.util.Scanner;
+public class App51 {
+    public static void main(String[] args) throws Exception{
+        Scanner sc=new Scanner(System.in);
+        int i = 0;
+       for (; i < 5; ){
+        System.out.println(i);
+        i++;
+       }
+    }
+}
